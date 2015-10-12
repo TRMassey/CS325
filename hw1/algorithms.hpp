8c5bdc49dc63e3a6a1c97c0da160e9f710525662
HerrickL <herrickl@onid.oregonstate.edu>
2015-10-12 15:55:06 -0700

    added algorithm3, and algorithm3_Midpoint, and algorithm4

--- hw1/algorithms.hpp
+++ hw1/algorithms.hpp
@@ -2,3 +2,7 @@
 
 int algorithm1(std::vector<int> inputArray);
 int algorithm2(std::vector<int> inputArray);
+int algo3_Midpoint(std::vector<int>&input, int start, int mid, int end);
+int algorithm3(std::vector<int>&input, int start, int end);
+int algorithm4(std::vector<int>&input);
+
