5bab64b8ad0da0a8aafacfc87b51dd4750bb0209
TRMassey <tararmassey@yahoo.com>
2015-10-12 17:37:12 -0700

    Reading and writing done, not checked

--- hw1/main.cpp
+++ hw1/main.cpp
@@ -6,40 +6,68 @@
 #include <vector>
 #include <cstring>
 
-int main(){
+int main(int argc, char* argv[]){
 
 	std::ifstream input;									// file pointer
 	std::ostream output;									// file pointer
-	std::vector<std::string> intialRead;		// read into this vector, vector of vectors
+	std::vector<std::string> intialRead;					// read into this vector, vector of vectors
 	std::vector<std::vector<int>> converted;				// read vector convereted to integers
 	std::vector<std::string> line;
+	std::string fileName;									// name of file to be read
 	int result;
 
-	// read the file
-	input.open(filenamehere);
-		// make sure we test for errors
 
+// Step One: Address possible conditions for selecting proper user input
 
-	// store file contents in a vector
-	while(!input.eof()){
-		input >> line;
-		initialRead.push_back(line);
+	// if a filename is provided
+	if(argc == 2){
+		fileName = argv[1];;
+	}
+	else{
+		fileName = "MSS_Problems.txt";
 	}
 
-	// close
+// Step Two: Open the file and address errors conccerning bad user input
+	input.open(fileName);
+	if(!input.is_open()){
+		std::cout << "ERROR: file unable to be opened\n";
+		return 0;
+	}
+
+
+// Step Three: Store the file contents in a vector for use
+	while(std::getline(input, line)){
+		input >> line;
+		initialRead.push_back(line);		// each line of input stores to one element in the vector
+	}
+
+
+// Step Four: Close and deal with closing errors
 	input.close();
+	if(input.is_open()){
+		std::cout << "ERROR: file unable to be properly closed\n";
+	}
 
 
-	// open the file for writing
-	output.open(filenamehere);
-		// test for errros
+// Step Five: Open the file for writing and deal with opening errors
+	fileName = "MSS_Results.txt";			// changed for output file
+	output.open(fileName);
+	if(!output.is_open()){
+		std::cout << "ERROR: file unable to be opened for writing"
+	}
 
 
-	// write each line and the result from the algo
+// Step Six: The file contents stored in a vector will be otuput to the
+// result file, then the line will be converted to only integers, the
+// algorithm will run on the converted line, return a result, and the result
+// will be written to the same output file.
 	for(int i=0; i < initialRead.size; i++){
 		output << intialRead[i];								// write the line
 		output << "\n";											// spacing
+
+		// NEED TO TOKENIZE AND CONVERT WITH ATOI ONCE COMMAS, SPACES, and BRACES ARE GONE
 		converted = intialRead[i] converted into ints. Maybe another function?
+
 		result = algorithm1(converted);							// call the function
 		output << result;										// write the answer
 		output << "\n\n";										// spacing
@@ -47,6 +75,12 @@ int main(){
 
 	// repeat for other three functions
 
-close writing
+// Step Last: Close and deal with closing errors
+	output.close();
+	if(output.is_open()){
+		std::cout << "ERROR: file unable to be properly closed\n";
+	}
+
 	return 0;
 }
+
