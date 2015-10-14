8ee22318efbebeb5355581e6493a3950f46e8d1c
TRMassey <tararmassey@yahoo.com>
2015-10-14 14:49:27 -0700

    added tokenize and random inputs of n

--- hw1/main.cpp
+++ hw1/main.cpp
@@ -12,9 +12,9 @@ int main(int argc, char* argv[]){
 	std::ostream output;									// file pointer
 	std::vector<std::string> intialRead;					// read into this vector, vector of vectors
 	std::vector<std::vector<int>> converted;				// read vector convereted to integers
-	std::vector<std::string> line;
-	std::string fileName;									// name of file to be read
-	int result;
+	std::vector<std::string> line;							// string for easier iteration through vector of strings
+	std::string fileName;									// name of file to be read or written to
+	int result;												// algorithm's answer
 
 
 // Step One: Address possible conditions for selecting proper user input
@@ -61,26 +61,226 @@ int main(int argc, char* argv[]){
 // result file, then the line will be converted to only integers, the
 // algorithm will run on the converted line, return a result, and the result
 // will be written to the same output file.
+
+	// algorithm One
+	std::cout << "\nRunning Analysis from file on Algorithm One";
 	for(int i=0; i < initialRead.size; i++){
 		output << intialRead[i];								// write the line
 		output << "\n";											// spacing
 
-		// NEED TO TOKENIZE AND CONVERT WITH ATOI ONCE COMMAS, SPACES, and BRACES ARE GONE
-		converted = intialRead[i] converted into ints. Maybe another function?
+		// Begin tokenization
+		line = initialRead[i];		// for easier iteration
+		std::string newLine;		// string without brackets
 
+		// strip brackets. New array made without brackets in newLine.
+		int k  0;
+		for(int j=0; j < line.length(); j++){
+			if(line[j] == '[' || line[j] == ']'){
+				continue;
+			}
+			else{
+				newLine[k] = line[j];
+				k++;
+			}
+		}
+
+		// tokenize based off commas
+		char* toTokenize = line.c_str();
+		char* token = strtok((char*) toTokenize, ",");
+		while(token != NULL){
+			int temp = atoi(token);
+			converted.push_back(temp);
+			token = strtok(NULL, ",");
+		}
+		
 		result = algorithm1(converted);							// call the function
 		output << result;										// write the answer
 		output << "\n\n";										// spacing
 	}
+	std::cout << "\nFile contents written.\n"
 
-	// repeat for other three functions
 
-// Step Last: Close and deal with closing errors
+	
+	// Algorithm two
+	std::cout << "\nRunning analysis of file on Algorithm Two";
+	for(int i=0; i < initialRead.size; i++){
+		output << intialRead[i];								// write the line
+		output << "\n";											// spacing
+
+		// Begin tokenization
+		line = initialRead[i];		// for easier iteration
+		std::string newLine;		// string without brackets
+
+		// strip brackets. New array made without brackets in newLine.
+		int k  0;
+		for(int j=0; j < line.length(); j++){
+			if(line[j] == '[' || line[j] == ']'){
+				continue;
+			}
+			else{
+				newLine[k] = line[j];
+				k++;
+			}
+		}
+
+		// tokenize based off commas
+		char* toTokenize = line.c_str();
+		char* token = strtok((char*) toTokenize, ",");
+		while(token != NULL){
+			int temp = atoi(token);
+			converted.push_back(temp);
+			token = strtok(NULL, ",");
+		}
+		
+		result = algorithm2(converted);							// call the function
+		output << result;										// write the answer
+		output << "\n\n";										// spacing
+	}
+	std::cout << "\nFile contents written.\n";
+
+	// algorithm Three
+	std::cout << "\nRunning Analysis on file for Algorithm Three";
+	for(int i=0; i < initialRead.size; i++){
+		output << intialRead[i];								// write the line
+		output << "\n";											// spacing
+
+		// Begin tokenization
+		line = initialRead[i];		// for easier iteration
+		std::string newLine;		// string without brackets
+
+		// strip brackets. New array made without brackets in newLine.
+		int k  0;
+		for(int j=0; j < line.length(); j++){
+			if(line[j] == '[' || line[j] == ']'){
+				continue;
+			}
+			else{
+				newLine[k] = line[j];
+				k++;
+			}
+		}
+
+		// tokenize based off commas
+		char* toTokenize = line.c_str();
+		char* token = strtok((char*) toTokenize, ",");
+		while(token != NULL){
+			int temp = atoi(token);
+			converted.push_back(temp);
+			token = strtok(NULL, ",");
+		}
+		
+		result = algorithm3(converted, 0, converted.size()-1);	// call the function
+		output << result;										// write the answer
+		output << "\n\n";										// spacing
+	}
+	std::cout << "\nFile contents written\n";
+
+
+	// algorithm Four
+	std::cout << "\nRunning Analysis on File for Algorithm Four"
+	for(int i=0; i < initialRead.size; i++){
+		output << intialRead[i];								// write the line
+		output << "\n";											// spacing
+
+		// Begin tokenization
+		line = initialRead[i];		// for easier iteration
+		std::string newLine;		// string without brackets
+
+		// strip brackets. New array made without brackets in newLine.
+		int k  0;
+		for(int j=0; j < line.length(); j++){
+			if(line[j] == '[' || line[j] == ']'){
+				continue;
+			}
+			else{
+				newLine[k] = line[j];
+				k++;
+			}
+		}
+
+		// tokenize based off commas
+		char* toTokenize = line.c_str();
+		char* token = strtok((char*) toTokenize, ",");
+		while(token != NULL){
+			int temp = atoi(token);
+			converted.push_back(temp);
+			token = strtok(NULL, ",");
+		}
+		
+		result = algorithm4(converted);							// call the function
+		output << result;										// write the answer
+		output << "\n\n";										// spacing
+	}
+	std::cout << "\nFile contents written\n";
+
+// Step Seven: Close and deal with closing errors
 	output.close();
 	if(output.is_open()){
 		std::cout << "ERROR: file unable to be properly closed\n";
 	}
 
+
+// PART TWO : Timing with random number generator
+	std::cout << "\n TIMING ANALYSIS BEGINNING FOR n AMOUNT OF RANDOM VALUES:";
+	std::vector<int> nArray = {100, 200, 400, 600, 900, 1000, 2000};	// sizes of array
+	std::vector<int> random;
+	double t1, t2;
+
+	// will run for as long as we have numbers of n input
+	for(i=0; i < nArray.size(); i++){
+
+		// we will run each n input size 10 times
+		for(k=0; k < 10; k++){
+
+			// make the array of n inputs, all random numbers
+			for(j=0; j < nArray[i]; j++){
+				random[j] = rand() % nArray.size() + 1;
+			}
+
+			// run each of the algorithms
+			std::cout << "Algorithm1:\n";
+			t1 = 1000 * clock() / CLOCKS_PER_SEC;
+			result = algorithm1(random);
+			t2 = 1000.0 * clock() / CLOCKS_PER_SEC;
+			cout << "\nRUNTIME FOR SIZE ";
+			cout << nArray[i];
+        	cout << " - Running time for Algorithm1 is: ";
+        	cout << result;
+        	cout << "\n";
+
+        			
+			std::cout << "Algorithm2:\n";
+			t1 = 1000 * clock() / CLOCKS_PER_SEC;
+			result = algorithm2(random);
+			t2 = 1000.0 * clock() / CLOCKS_PER_SEC;
+			cout << "\nRUNTIME FOR SIZE ";
+			cout << nArray[i];
+        	cout << " - Running time for Algorithm1 is: ";
+        	cout << result;
+        	cout << "\n";
+
+        		
+			std::cout << "Algorithm3:\n";
+			t1 = 1000 * clock() / CLOCKS_PER_SEC;
+			result = algorithm3(random, 0, random.size()-1);
+			t2 = 1000.0 * clock() / CLOCKS_PER_SEC;
+			cout << "\nRUNTIME FOR SIZE ";
+			cout << nArray[i];
+        	cout << " - Running time for Algorithm1 is: ";
+        	cout << result;
+        	cout << "\n";
+
+			std::cout << "Algorithm4:\n";
+			t1 = 1000 * clock() / CLOCKS_PER_SEC;
+			result = algorithm4(random);
+			t2 = 1000.0 * clock() / CLOCKS_PER_SEC;
+			cout << "\nRUNTIME FOR SIZE ";
+			cout << nArray[i];
+        	cout << " - Running time for Algorithm1 is: ";
+        	cout << result;
+        	cout << "\n";
+        }
+	}
 	return 0;
 }
 
