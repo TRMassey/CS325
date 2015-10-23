4f1eb9fdecc8c9515638ebe18286b73f83b38ab8
HerrickL <herrickl@onid.oregonstate.edu>
2015-10-22 18:38:28 -0700

    edited chageslow

--- hw2/algorithms.cpp
+++ hw2/algorithms.cpp
@@ -2,6 +2,8 @@
 #include <iostream>
 #include <vector>
 #include <assert.h>
+#include <algorithm>
+#include <limits.h>
 
 /**********************************************************
 Funcction: changeslow
@@ -10,26 +12,35 @@ Time Complexity: exponential O(2^n)
 Input: sorted array/vector of coins in register, int end of
 array, value of change that needs to be made, array/vector of
 used coins.
-Notes: Adapted from pseudocode and discussion found at
-http://www.algorithmist.com/index.php/Coin_Change#Recursive_Formulation
+Notes: Adapted from pseudocode, code, and discussion found at
+http://www.algorithmist.com/index.php/Min-Coin_Change
+http://www.geeksforgeeks.org/find-minimum-number-of-coins-that-make-a-change/
 **********************************************************/
 int changeslow(std::vector<int> coins, int index, int value, std::vector<int> &used){
+	int count = INT_MAX;			//to compare total result to sub result
+	int subCount = 0;				//holds sub-results from recursion
+
 	//error if no coins
 	assert(coins.size() != 0); 
 
-	//base case - if recursive call matches value
+	//base case - finished making change
 	if(value == 0){
-		//std::cout << coins[index] << std::endl;
-		used[index] += 1;			//increment coin use at index
-		return 1;					//used for count increment
-	}
-	if(value < 0 || index <= 0){
-		return 0;					//coin doesn't work, don't count
+		return 0;		// don't increment count
 	}
 
 	//else divide into subproblems- for each i<k
-	//find the min number of (coins needed to make i cents) + (coins needed to make k-i cents)
-	return changeslow(coins, index-1, value, used) + changeslow(coins, index, value-coins[index-1], used);
+	for(int index = 0; index < coins.size(); index++){
+			//find the min number of coins needed to make i cents
+			if(coins[index] <= value){
+				//find the min number of coins needed to make K-i cents
+				subCount = changeslow(coins, coins.size(), value - coins[index], used);
+			}
+			//chose the i that minimizes this sum
+			if(subCount != INT_MAX && subCount + 1 < count){
+				count = subCount + 1;
+			}
+	}
+	return count;
 }
 
 
@@ -82,8 +93,7 @@ titled, "Dynamic Programming | Set 7 (Coin Change)".
 int changedp(std::vector<int> coins, int value, std::vector<int> &used){
 	int count = 0;						// coins used
 	//std::map<int, int> table (1, value);//table of sub-values
-	std::vector<int> table (value+1, 0);	//table of sub-values
-	std::cout << table.size() << std::endl;
+	std::vector<int> table (value+1, 0);//table of sub-values
 	table[0] = 1;
 
 	//error if no coins
@@ -93,6 +103,7 @@ int changedp(std::vector<int> coins, int value, std::vector<int> &used){
 	for(int index = 0; index < coins.size(); index++){
 		for(int coin = coins[index]; coin <= value; coin++){
 			table[coin] += table[coin-coins[index]];
+			std:: cout << coin << std::endl;
 			used[index] += 1;
 		}
 	}
