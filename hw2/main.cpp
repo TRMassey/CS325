1e9fa4cc6b6067a7739dd9fc5ea934cc567b1e73
TRMassey <tararmassey@yahoo.com>
2015-10-27 12:36:12 -0700

    Timing added.

--- hw2/main.cpp
+++ hw2/main.cpp
@@ -10,6 +10,7 @@
 #include <iomanip>
 #include <cstdlib>
 #include <numeric>
+#include <time.h>
 #include <algorithm>    // std::merge, std::sort
 
 
@@ -23,6 +24,7 @@ int main(){
     std::string outFilename;
     std::ifstream input;
     std::ofstream output;
+    std::ofstream outputTime;
     std::vector<std::string> reading;       // holds lines from input file
     std::vector<int> converted;             // holds actual int values
     std::string line;
@@ -30,6 +32,8 @@ int main(){
     int val;                                // converted value to be coin-changed
     int result;                             // result from algo call
     int choice;
+    float t1, t2;
+    float total;
 
     std::cout << "Enter the name of the file to open: ";
     std::cin >> inFilename;
@@ -213,7 +217,157 @@ int main(){
         }
     }
 
+// timing section here
 
+    char charChoice;
+    std::cout << "\nPerforming Timing Results? Y or N: ";
+    std::cin >> charChoice;
+    if(charChoice != 'y' && charChoice != 'Y'){
+        return 0;
+    }
+    else{
+         if(checkMe[0] == '['){
+        for (int i = 0; i < reading.size(); i++) {
+            line = reading[i];
+
+            // positive lines contain the array
+            if (i % 3 == 0) {
+                // replace all non-spaces with spaces
+                std::size_t found = line.find_first_of("[,]");
+
+                while (found!=std::string::npos) {
+                    line[found]=' ';
+                    found=line.find_first_of("[,]",found+1);
+                }
+
+                // tokenize based off spaces
+                const char* toTokenize = line.c_str();
+                char* token = strtok((char*) toTokenize, " ");
+                converted.clear();
+                while(token != NULL){
+                    int temp = atoi(token);
+
+                    converted.push_back(temp);
+                    token = strtok(NULL, " ");
+                }
+                //second array                
+                while (found!=std::string::npos) {
+                    line[found]=' ';
+                    found=line.find_first_of("[,]",found+1);
+                }
+
+                // tokenize based off spaces
+                toTokenize = line.c_str();
+                token = strtok((char*) toTokenize, " ");
+                while(token != NULL){
+                    int temp = atoi(token);
+                    //if not in vector already, add, sort
+                    if(std::find(converted.begin(), converted.end(), temp) == converted.end()){
+                        converted.push_back(temp);
+                        std::sort(converted.begin(), converted.end());
+                    }
+                    token = strtok(NULL, " ");
+                }
+                // now that converted holds all the integers, pull the value
+                // being searched for from the next line
+                line = reading[i+2];
+                val = atoi(line.c_str());
+
+                // call sorting function
+                std::vector<int> used (converted.size(), 0);
+
+                if(choice == 0){
+                     t1 = 1000 * clock() / CLOCKS_PER_SEC;
+                    used = changeslow(converted, val, converted.size());
+                    result = arraySum(used, used.size());
+                     t2 = 1000.0 * clock() / CLOCKS_PER_SEC;
+                     total = (t2-t1); // divide answer by loops
+                    outputTime << total << "\n";
+                }
+                else if(choice == 1){
+                     t1 = 1000 * clock() / CLOCKS_PER_SEC;
+                    for(int j = 0; j < 50000; j++){
+                        result = changegreedy(converted, val, used);
+                    }
+                     t2 = 1000.0 * clock() / CLOCKS_PER_SEC;
+                     total = (t2-t1)/50000; // divide answer by loops
+                    outputTime << total << "\n";
+                }
+                else if(choice == 2) {            
+                    t1 = 1000 * clock() / CLOCKS_PER_SEC; 
+                    for(int j = 0; j < 50000; j++){
+                         result = changedp(converted, val, used);
+                    }
+                     t2 = 1000.0 * clock() / CLOCKS_PER_SEC;
+                     total = (t2-t1)/50000; // divide answer by loops
+                    outputTime << total << "\n";
+                }
+            }
+        }
+    }
+    else{
+        for (int i = 0; i < reading.size(); i++) {
+            line = reading[i];
+
+            // positive lines contain the array
+            if (i % 2 == 0) {
+                // replace all non-spaces with spaces
+                std::size_t found = line.find_first_of("[,]");
+
+                while (found!=std::string::npos) {
+                    line[found]=' ';
+                    found=line.find_first_of("[,]",found+1);
+                }
+
+                // tokenize based off spaces
+                const char* toTokenize = line.c_str();
+                char* token = strtok((char*) toTokenize, " ");
+                converted.clear();
+                while(token != NULL){
+                    int temp = atoi(token);
+
+                    converted.push_back(temp);
+                    token = strtok(NULL, " ");
+                }
+
+                // now that converted holds all the integers, pull the value
+                // being searched for from the next line
+                line = reading[i+1];
+                val = atoi(line.c_str());
+
+                // call sorting function
+                std::vector<int> used (converted.size(), 0);
+//CHANGE ALGO HERE    
+                if(choice == 0){
+                     t1 = 1000 * clock() / CLOCKS_PER_SEC;
+                    used = changeslow(converted, val, converted.size());
+                    result = arraySum(used, used.size());
+                     t2 = 1000.0 * clock() / CLOCKS_PER_SEC;
+                     total = (t2-t1); // divide answer by loops
+                    outputTime << total << "\n";
+                }
+                else if(choice == 1){
+                     t1 = 1000 * clock() / CLOCKS_PER_SEC;
+                    for(int j = 0; j < 50000; j++){
+                        result = changegreedy(converted, val, used);
+                    }
+                     t2 = 1000.0 * clock() / CLOCKS_PER_SEC;
+                     total = (t2-t1)/50000; // divide answer by loops
+                    outputTime << total << "\n";
+                }
+                else if(choice == 2) {            
+                    t1 = 1000 * clock() / CLOCKS_PER_SEC; 
+                    for(int j = 0; j < 50000; j++){
+                         result = changedp(converted, val, used);
+                    }
+                     t2 = 1000.0 * clock() / CLOCKS_PER_SEC;
+                     total = (t2-t1)/50000; // divide answer by loops
+                    outputTime << total << "\n";
+                }
+            }
+        }
+    }
+}
 	/* All functions require a vector for register coins AND a zeroed out
 	  vector for marking coins used.  Must clear used after each function
 	  call or create new used */
