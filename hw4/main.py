debfcfc9a02ffb30bd5d0bea55a4ab18fc949da6
Diana O'Haver <c4t3rp1ll4r@gmail.com>
2015-11-28 20:05:52 -0600

    Moved skeleton to main

--- hw4/main.py
+++ hw4/main.py
@@ -5,31 +5,42 @@
 import sys
 import time
 
-# Your program must:
-# Accept problem instances on the command line
 
-f = open(sys.argv[1], "r")
-inputname = sys.argv[1]
-contents = f.read()
-f.close()
 
-# timer needed
-start = time.time()
-# this is where we would call the algorithm
-tour = "algorithm here"
 
-end = time.time()
 
-print "Time elapsed:",(end - start), "seconds"
+def main():
+    # Your program must:
+    # Accept problem instances on the command line
 
-print "Tour length:", tour
+    f = open(sys.argv[1], "r")
+    inputname = sys.argv[1]
+    contents = f.read()
+    f.close()   
 
-# Name the output file as the input file's name with .tour appended 
-outputname = inputname + ".tour"
+	# timer needed
+    start = time.time()
+    # this is where we would call the algorithm
+    tour = "algorithm here"
 
-out = open(outputname, "w")
+    end = time.time()
 
-out.write(tour)
+    print "Time elapsed:",(end - start), "seconds"
 
-# need to write the path for the above length here
+    print "Tour length:", tour
 
+    # Name the output file as the input file's name with .tour appended 
+    outputname = inputname + ".tour"
+
+    out = open(outputname, "w")
+
+    out.write(tour)
+
+    # need to write the path for the above length here
+
+
+
+
+
+if __name__ == "__main__":
+    main()
