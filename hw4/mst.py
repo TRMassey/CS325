c64154f9641e22a844fc83ca79a1087f45e2d465
HerrickL <herrickl@onid.oregonstate.edu>
2015-11-29 17:26:49 -0800

    mst.py ready for mst_prim testing

--- hw4/mst.py
+++ hw4/mst.py
@@ -18,7 +18,7 @@ def graph_edges(G):
 	for vertex1 in range(0, len(G)):
 		lowRange += 1
 		#iterate through all end points
-		for vertex2 in range(lowRange, len(G)):
+		for vertex2 in [x for x in xrange(len(G)) if x != vertex1]:
 			#this is from assignment description
 			edges[(vertex1,vertex2)] = nearest_int(math.sqrt(pow(G[vertex1][0]-G[vertex2][0], 2)+pow(G[vertex1][1]-G[vertex2][1],2)))
 	#dict of key=(vertex#,vertex#), value = edge weight 
@@ -43,22 +43,20 @@ def mst_prim(G):
 	for v in range(0, len(G)):
 		Q.append(v)
 	minList = [None]*len(Q)	#this will be used later to go between Q and key
+	Qsize = len(Q)
 	#while Q is not empty
 	while Q:
 		#find smallest key of applicble Qs
-		minList = [None]*len(minList)
+		minList = [float("inf")]*Qsize
 		for num in Q:
-			if key[num]:
-				minList[num] = key[num]
-			else:
-				minList[num] = float("inf")
+			minList[num] = key[num]
 		u = min(u for u in minList)
 		#remove Q that was used
 		cur = (key.index(u))
-		Q.pop(cur)
+		Q.remove(cur)
 
 		if parent[cur] != None:
-			A.push(cur, parent[cur])
+			A.append((cur, parent[cur]))
 
 		#in this assignment, all verts are neighbors
 		for neighbor in [x for x in xrange(len(G)) if x != cur]:
@@ -69,14 +67,18 @@ def mst_prim(G):
 	return A
 
 
-G = ((0,0), (1,3), (6,0))
-print mst_prim(G)
-
 '''
 # Simple test from assignment description for graph_edges and nearest_int
 # should output {(0,1): 3, (1,2): 6, (0,2): 6}
 G = ((0,0), (1,3), (6,0))
 print graph_edges(G)
+'''
+
+
+'''
+# tests for mst_prim go here
+
+
 '''
 
 
