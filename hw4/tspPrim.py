a7ff9367b112a6f2120df32c28cfde951e9dfa82
HerrickL <herrickl@onid.oregonstate.edu>
2015-12-03 19:44:39 -0800

    2-opt done

--- hw4/tspPrim.py
+++ hw4/tspPrim.py
@@ -7,18 +7,18 @@ import operator
 
 def makeGraph():
     # make a list of the values as they are read in
-	# since the number of the cities corresponds with its position in the list, that value is not saved
-	coordList = list()
-	
-	filename = sys.argv[1]
-	
-	with open(filename) as f:
-		for line in f:
-			splitter = line.split()
-			coord = (splitter[1], splitter[2])
-			coordList.append(coord)
+    # since the number of the cities corresponds with its position in the list, that value is not saved
+    coordList = list()
+    
+    filename = sys.argv[1]
+    
+    with open(filename) as f:
+        for line in f:
+            splitter = line.split()
+            coord = (splitter[1], splitter[2])
+            coordList.append(coord)
 
-	return coordList
+    return coordList
 
 
 
@@ -37,7 +37,7 @@ def nearestInt(num):
 
 
 def calcEdge(x,y):
-	return nearestInt(math.sqrt(((int(x[0])-int(y[0]))**2)+((int(x[1])-int(y[1]))**2)))
+    return nearestInt(math.sqrt(((int(x[0])-int(y[0]))**2)+((int(x[1])-int(y[1]))**2)))
 
 
 
@@ -61,15 +61,16 @@ def graphEdges(G):
 
 
 
+
 def totalDistance(path, edges):
-	distance = 0
-	curVert = path[0]
-	#iterate through list starting at index 1 (index 0 is distance of 0):
-	for vertex in path [2:]:
-		distance += edges[curVert, vertex]
-		curVert = vertex
-	distance += edges[curVert, path[0]]
-	return distance
+    distance = 0
+    curVert = path[0]
+    #iterate through list starting at index 1 (index 0 is distance of 0):
+    for vertex in path [2:]:
+        distance += edges[curVert, vertex]
+        curVert = vertex
+    distance += edges[curVert, path[0]]
+    return distance
 
 
 
@@ -77,9 +78,14 @@ def totalDistance(path, edges):
 # for TSP problems, where all vertices connect to all other vertices.
 # Parmeters: set edges in dict (start, end) = weight
 # Return: a list of edges in min spanning tree ex: [(0,1),(1,2),(3,4)]
-def mstPrim(edges, G, Q):
+def mstPrim(edges, G):
     vertList = list()
     edgeList = list()
+
+    #create list of vertices
+    Q = list()
+    for vertex in range(0, len(G)):
+        Q.append(vertex)
 
     #each key is set to infinity
     key = [float("inf")]*len(G)
@@ -102,71 +108,85 @@ def mstPrim(edges, G, Q):
         minEdge = float("inf")
         #find smallest key of applicble Qs, edges are pre-sorted
         for vertex in vertList:
-        	for edge in edges:
-        		#start is vertex
-        		if edge[0][0] == vertex:
-        			if edge[0][1] in Q:
-        				minEdge = edges[1]
-        				minV1 = vertex
-        				minV2 = edge[0][1]
-        				break # found has to be solution
-        		#destination is vertex
-        		elif edge[0][1] == vertex:
-        			if edge[0][0] in Q:
-        				minEdge = edges[1]
-        				minV1 = edge[0][1]
-        				minV2 = edge[0][0]
-        				break #found has to be solution
+            for edge in edges:
+                #start is vertex
+                if edge[0][0] == vertex:
+                    if edge[0][1] in Q:
+                        minEdge = edges[1]
+                        minV1 = vertex
+                        minV2 = edge[0][1]
+                        break # found has to be solution
+                #destination is vertex
+                elif edge[0][1] == vertex:
+                    if edge[0][0] in Q:
+                        minEdge = edges[1]
+                        minV1 = edge[0][1]
+                        minV2 = edge[0][0]
+                        break #found has to be solution
         vertList.append(minV2)
         Q.remove(minV2)
         edgeList.append((minV1,minV2))
     return (vertList, edgeList)
 
 
-def buildPath(verticies, vertPath):
-	for vertex in range(len(vertices) -3):
-		print vertex
+#uses 2-opt
+def buildPath(vertices, vertPath, edges):
+    #go through the path or mst backwards
+    for vertex in range(len(vertPath) -3):
+        order1 = 0
+        order2 = 0
+        #2-opt rule: swap if [1,3,2,4] < [1,2,3,4]
+        order1 = edges[vertex, vertex+2]+edges[vertex+2, vertex+1]+edges[vertex+1, vertex+3]
+        order2 = edges[vertex, vertex+1]+edges[vertex+1, vertex+2]+edges[vertex+2, vertex+3]
+        if(order1 < order2): #2-opt swap
+        	#swap 2 and 3
+        	temp = vertPath[vertex+1]
+        	vertPath[vertex+1] = vertPath[vertex+2]
+        	vertPath[vertex+2] = temp
+    return vertPath
 
 
 
 
 
 def tsp(G):
-	#get list of vertices
-	vertices = list()
-	for vertex in range(0, len(G)):
-		vertices.append(vertex)
+    #get list of vertices
+    vertices = list()
+    for vertex in range(0, len(G)):
+        vertices.append(vertex)
 
-	#get edges for graph
-	edges = graphEdges(G)
-	#sort dictionary by values/edges
-	tempEdges = sorted(edges.items(), key=lambda x: x[1])
+    #get edges for graph
+    edges = graphEdges(G)
+    #sort dictionary by values/edges
+    tempEdges = sorted(edges.items(), key=lambda x: x[1])
 
-	#make prims mst
-	mst = mstPrim(tempEdges, G, vertices)
-	vertPath = mst[0]	#vert in order from mst
-	edgePath = mst[1]	#edges in order from mst
-	
-	#find path
-	#establish current distance after mst
-	distance = 0
-	for edge in edgeList:
-		distance += edges[edge]
+    #make prims mst
+    mst = mstPrim(tempEdges, G)
+    vertPath = mst[0]   #vert in order from mst
+    edgePath = mst[1]   #edges in order from mst
 
-	#establish exact min path
-	minPathFound = False
-	while not minPathFound:
-		#build path
-		vertPath = buildPath(verticies, vertPath)
-		#check if better
-		if curDist < distance:
-			distance = curDist
-			vertPath = curVerts
-		#min has been found
-		else:
-			minPathFound = True
+    #find path
+    #establish current distance after mst
+    distance = 0
+    for edge in edgePath:
+        distance += edges[edge]
 
-	return results
+    #establish exact min path
+    minPathFound = False
+    while not minPathFound:
+        #build path
+        vertPath = buildPath(vertices, vertPath, edges)
+        curVerts = []
+        curDist = 0
+        #check if better
+        if curDist < distance:
+            distance = curDist
+            vertPath = curVerts
+        #min has been found
+        else:
+            minPathFound = True
+
+    return results
 
 
 
